e0896f170b2462538cbd1f44ad8a97d8cd345a52
Thanmai-20WH1A1265 <85935875+Thanmai-20WH1A1265@users.noreply.github.com>
2021-07-18 09:39:44 -0700

    Add files via upload

---
+++ acc time.cpp
@@ -0,0 +1,10 @@
+/*a.	A building has 10 floors with a floor height of 3 meters each.
+ A ball is dropped from the top of the building. Find the time taken by the ball to reach each floor.
+  (Use the formula s = ut+(1/2)at^2 where u and a are the initial velocity in m/sec (= 0) and acceleration in m/sec^2 (= 9.8 m/s^2)).*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int u,s,t;
+	
+}
