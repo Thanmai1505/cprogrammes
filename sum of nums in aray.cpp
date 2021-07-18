d1df8a638cf0a61732c8ac7aaf3c4a4a45e205e7
Thanmai-20WH1A1265 <85935875+Thanmai-20WH1A1265@users.noreply.github.com>
2021-07-18 09:45:43 -0700

    Add files via upload

---
+++ sum of nums in aray.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int A[10],i,sum=0;
+	printf(" \nenter 10 numbers:");
+	for(i=0;i<10;i++)
+	scanf("%d",&A[i]);
+	printf("\n");
+	sum = sum+A[i];
+	printf("\n %d",sum);
+	return 0;
+	
+	 
+}
---
+++ sum of nums.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,sum=0;
+	while(a<=10);
+	{
+		printf("\n %d",a);
+		sum+=a;
+		a+=1;
+	
+	}
+	printf("\n sum is %d",sum+=a);
+	
+}
+	
+
