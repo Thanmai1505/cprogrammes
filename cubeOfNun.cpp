aaafa2662f1832c0de6f373b7a7c8e73046d049f
Thanmai-20WH1A1265 <85935875+Thanmai-20WH1A1265@users.noreply.github.com>
2021-07-20 08:06:58 +0530

    Add files via upload

---
+++ cubeOfNun.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a,res;
+	printf("\n Enter a num:");
+	scanf("%f",&a);
+	res = cube('a');
+	//res = a*a*a;
+	printf("\n %f",res);
+	return 1;
+	
+	
+}
